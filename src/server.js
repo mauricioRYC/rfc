e5197b49ac4aec190fe90f205fb9617e225145a7
Mauricio <Mauricio@DESKTOP-40S0MI4>
2024-07-04 09:52:30 -0600

    PDF

--- src/server.js
+++ src/server.js
@@ -3,115 +3,159 @@ const path = require("path");
 const ejs = require("ejs");
 const QRCode = require("qrcode");
 const fs = require("fs").promises;
-const bwipjs = require('bwip-js');
+const bwipjs = require("bwip-js");
 const puppeteer = require("puppeteer");
 
 const app = express();
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "assets/views"));
+app.set("views", path.join(__dirname, "src/assets/views"));
 app.use(express.static(path.resolve(__dirname, "assets")));
 
 app.get("/", async (req, res) => {
     const { idCIF, rfc, nombreCompleto, fecha } = req.query;
-    console.log(idCIF, rfc, " jajaja");
     const url = `https://siat.sat.gob.mx/app/qr/faces/pages/mobile/validadorqr.jsf?D1=10&D2=1&D3=${idCIF}_${rfc}`;
-    // const qrText = await QRCode.toString(url);
     const qrPath = await QRCode.toString(url, {
         type: "svg",
         size: 200,
     });
     const qr = "qrcode.svg";
+    const barcode = "barcode.png";
     const svgFilePath = path.join(__dirname, "assets/svg", qr);
-    await fs.writeFile(svgFilePath, qrPath);
-
-    const barcodeFilePath = path.join(__dirname, 'assets/barcodes', 'barcode.png');
-    await generateBarcode(rfc, barcodeFilePath);
-
-
-    res.render("index", {
-        qrPath: qr,
-        barcodePath: 'assets/barcodes/barcode.png',
-        rfc: rfc,
+    const barcodeFilePath = path.join(__dirname, "assets/barcodes", barcode);
+    const data = {
+        qrPath: `/svg/${qr}`,
+        barcodePath: `/barcodes/${barcode}`,
+        rfc,
         nombre: nombreCompleto,
-        idCIF: idCIF,
-        fecha: fecha,
-    });
-});
-
-async function generateBarcode(text, outputPath) {
-    return new Promise((resolve, reject) => {
-        bwipjs.toBuffer({
-            bcid: 'code128',       // Barcode type
-            text: text,            // Text to encode
-            scale: 3,              // 3x scaling factor
-            height: 10,            // Bar height, in millimeters
-            includetext: true,     // Show human-readable text
-            textxalign: 'center',  // Always good to set this
-        }, function (err, png) {
-            if (err) {
-                return reject(err);
-            }
-            fs.writeFile(outputPath, png)
-                .then(resolve)
-                .catch(reject);
-        });
-    });
-}
-
-/**
- *  const { idCIF, rfc } = req.params;
-app.get("/generate-pdf", async (req, res) => {
+        idCIF,
+        fecha,
+    };
     try {
-        const { idCIF, rfc, nombreCompleto, fecha } = req.query;
-        const url = `https://siat.sat.gob.mx/app/qr/faces/pages/mobile/validadorqr.jsf?D1=10&D2=1&D3=${idCIF}_${rfc}`;
-        const qrPath = await QRCode.toString(url, {
-            type: "svg",
-            size: 200,
-        });
-        const qr = "qrcode.svg";
-        const svgFilePath = path.join(__dirname, "assets/svg", qr);
         await fs.writeFile(svgFilePath, qrPath);
-        const html = await res.render("index.ejs", {
-            idCIF,
-            rfc,
-            nombre: nombreCompleto,
-            fecha,
+        await generateBarcode(rfc, barcodeFilePath);
+        const template = path.resolve(__dirname, "./assets/views/index.ejs");
+        ejs.renderFile(template, data, async function (err, html) {
+            if (err) {
+                console.log("ERROR EN IF: ", err, "<=");
+            }
+            const browser = await puppeteer.launch({ headless: true });
+            const page = await browser.newPage();
+            await page.pdf({
+                path: "./assets/rfc.pdf",
+                format: "A4",
+            });
+            await browser.close();
         });
-        const browser = await puppeteer.launch({
-            headless: false, 
-            args: ["--start-maximized"], 
-        });
-
-        console.log("Navegador abierto.");
-
-        const page = await browser.newPage();
-        await page.setContent(html, { waitUntil: "networkidle0" });
-        console.log("Contenido de la página establecido.");
-
-        const pdf = await page.pdf({
-            format: "letter",
-            printBackground: true,
-            margin: {
-                top: "0.5in",
-                right: "0.5in",
-                bottom: "0.5in",
-                left: "0.5in",
-            },
-        });
-
-        await browser.close();
-        console.log("Navegador cerrado.");
-
-        // Configurar la respuesta del PDF
-        res.contentType("application/pdf");
-        res.send(pdf);
     } catch (error) {
-        console.log(error, " error en algo la ptm");
+        console.error("Error al generar el PDF:", error);
         res.status(500).send("Error al generar el PDF");
     }
 });
 
+// app.get("/", async (req, res) => {
+//     const { idCIF, rfc, nombreCompleto, fecha } = req.query;
+//     console.log(idCIF, rfc, " jajaja");
+//     const url = `https://siat.sat.gob.mx/app/qr/faces/pages/mobile/validadorqr.jsf?D1=10&D2=1&D3=${idCIF}_${rfc}`;
+//     // const qrText = await QRCode.toString(url);
+//     const qrPath = await QRCode.toString(url, {
+//         type: "svg",
+//         size: 200,
+//     });
+//     const qr = "qrcode.svg";
+//     const barcode = "barcode.png";
+//     const svgFilePath = path.join(__dirname, "assets/svg", qr);
+//     await fs.writeFile(svgFilePath, qrPath);
+
+//     const barcodeFilePath = path.join(__dirname, "assets/barcodes", barcode);
+//     await generateBarcode(rfc, barcodeFilePath);
+//     const html = await ejs.renderFile(
+//         path.join(__dirname, "assets/views", "index.ejs"),
+//         {
+//             qrPath: `/svg/${qr}`,
+//             barcodePath: `/barcodes/${barcode}`,
+//             rfc: rfc,
+//             nombre: nombreCompleto,
+//             idCIF: idCIF,
+//             fecha: fecha,
+//         },
+//     );
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.setContent(html);
+//     const pdfBuffer = await page.pdf({
+//         format: "A4",
+//         printBackground: true,
+//     });
+//     await browser.close();
+
+//     // Enviar el PDF como respuesta
+//     res.contentType("application/pdf");
+//     res.send(pdfBuffer);
+// });
+
+app.get("/generar-pdf", async (req, res) => {
+    const { idCIF, rfc, nombreCompleto, fecha } = req.query;
+    const url = `https://siat.sat.gob.mx/app/qr/faces/pages/mobile/validadorqr.jsf?D1=10&D2=1&D3=${idCIF}_${rfc}`;
+    const qrPath = await QRCode.toString(url, {
+        type: "svg",
+        size: 200,
+    });
+    const qr = "qrcode.svg";
+    const barcode = "barcode.png";
+    const svgFilePath = path.join(__dirname, "assets/svg", qr);
+    await fs.writeFile(svgFilePath, qrPath);
+    const barcodeFilePath = path.join(__dirname, "assets/barcodes", barcode);
+    await generateBarcode(rfc, barcodeFilePath);
+    const html = await ejs.renderFile(
+        path.join(__dirname, "assets/views", "index.ejs"),
+        {
+            qrPath: `/svg/${qr}`,
+            barcodePath: `/barcodes/${barcode}`,
+            rfc: rfc,
+            nombre: nombreCompleto,
+            idCIF: idCIF,
+            fecha: fecha,
+        },
+    );
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    const { width, height } = await page.evaluate(() => {
+        return {
+            width: window.screen.availWidth,
+            height: window.screen.availHeight,
+        };
+    });
+    await page.setViewport({ width, height });
+    await page.setContent(html, { waitUntil: "networkidle0", timeout: 60000 });
+    await page.waitForTimeout(2000);
+    const pdf = await page.pdf({ format: "letter" });
+    await browser.close();
+
+    res.contentType("application/pdf");
+    res.send(pdf);
+});
+
+async function generateBarcode(text, outputPath) {
+    return new Promise((resolve, reject) => {
+        bwipjs.toBuffer(
+            {
+                bcid: "code128",
+                text: text,
+                scale: 3,
+                height: 10,
+                includetext: true,
+                textxalign: "center",
+            },
+            function (err, png) {
+                if (err) {
+                    return reject(err);
+                }
+                fs.writeFile(outputPath, png).then(resolve).catch(reject);
+            },
+        );
+    });
+}
 app.listen(8080, () => {
     console.log(`express server running on 8080`);
 });
